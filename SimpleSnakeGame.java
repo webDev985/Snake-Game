3d403b056c2131bb223e1fdf4ec07c38942732a7
Shivam Kumar <152677524+webDev985@users.noreply.github.com>
2024-10-05 00:49:02 +0530

    Update SimpleSnakeGame.java

--- SimpleSnakeGame.java
+++ SimpleSnakeGame.java
@@ -42,7 +42,7 @@ public class SimpleSnakeGame extends JPanel implements ActionListener {
 
     private void initBoard() {
         addKeyListener(new TAdapter());
-        setBackground(Color.blue);
+        setBackground(Color.black);
         setFocusable(true);
         setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
         initGame();
