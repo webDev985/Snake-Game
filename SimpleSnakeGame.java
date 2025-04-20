4356ba47391cfd8bba8340d31e4c4ccda5f951ae
Shivam Kumar <152677524+webDev985@users.noreply.github.com>
2025-04-20 17:35:05 +0530

    Update SimpleSnakeGame.java

--- SimpleSnakeGame.java
+++ SimpleSnakeGame.java
@@ -18,7 +18,7 @@ public class SimpleSnakeGame extends JPanel implements ActionListener {
     private final int B_HEIGHT = 300;
     private final int DOT_SIZE = 10;
     private final int ALL_DOTS = 900;
-    private final int RAND_POS = 30;
+    private final int RAND_POS = 29;
     private final int DELAY = 100;
 
     private final int x[] = new int[ALL_DOTS];
