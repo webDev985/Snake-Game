1b97c0e57fa214bfd69bf7a4e170540c5d37e8b3
Shivam Kumar <152677524+webDev985@users.noreply.github.com>
2025-04-24 22:40:35 +0530

    Update SnakeGameFrame.java

--- SnakeGameFrame.java
+++ SnakeGameFrame.java
@@ -8,7 +8,7 @@ public class SnakeGameFrame extends JFrame {
         pack();
 
         setTitle("Game of Snakes");
-        setLocationRelativeTo(null);
+        setLocationRelativeTo();
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
