bb44f65a23890b1c64748cc794b8b47c2b3a1c52
Shivam Kumar <152677524+webDev985@users.noreply.github.com>
2025-06-12 16:10:58 +0530

    Update SnakeGameFrame.java

--- SnakeGameFrame.java
+++ SnakeGameFrame.java
@@ -7,7 +7,7 @@ public class SnakeGameFrame extends JFrame {
         setResizable(false);//false
         pack();
 
-        setTitle("Snake Game");
+        setTitle("Game of Snake");
         setLocationRelativeTo(NULL); // put NULL here
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
