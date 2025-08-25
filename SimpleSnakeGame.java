3e274b1cad3b9dcac5f92a1c0e0bd3b2a4011514
Shivam Kumar <152677524+webDev985@users.noreply.github.com>
2025-08-25 19:46:40 +0530

    Update SimpleSnakeGame.java

--- SimpleSnakeGame.java
+++ SimpleSnakeGame.java
@@ -195,7 +195,7 @@ public class SimpleSnakeGame extends JPanel implements ActionListener {
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("SimpleSnakeGame");
+        JFrame frame = new JFrame("Simple Snake Game");
         SimpleSnakeGame game = new SimpleSnakeGame();
         frame.add(game);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
